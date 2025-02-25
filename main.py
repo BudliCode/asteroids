6b9d239c03e5d0f3b7b810dbc13ef7a6565387af
BudliCode <birger@luland.de>
2025-02-25 19:24:34 +0100

    draw player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FRAMERATE
 import pygame as pg
+
+from player import Player
 
 
 def main():
@@ -7,14 +9,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pg.init()
+
+    clock = pg.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
 
         pg.display.flip()
+        dt = clock.tick(FRAMERATE) / 1000
 
 
 if __name__ == "__main__":
